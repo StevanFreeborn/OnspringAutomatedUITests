bfa1d6f520babf1ef42467ecfeda276c511186b3
StevanFreeborn <sfreeborn7@gmail.com>
2022-07-12 23:59:06 -0500

    included configurationfactory class in source control

---
+++ Configuration/ConfigurationFactory.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.Extensions.Configuration;
+
+namespace OnspringAutomatedUITests.Configuration
+{
+    public static class ConfigurationFactory
+    {
+        public static IConfiguration GetConfig()
+        {
+            return new ConfigurationBuilder()
+                .SetBasePath(Directory.GetCurrentDirectory())
+                .AddJsonFile("Configuration/appsettings.json")
+                .Build();
+        }
+    }
+}
