3ff89e8f3b0d2edcb626d5064d1c917e4862910c
StevanFreeborn <sfreeborn7@gmail.com>
2022-07-14 19:14:33 -0500

    changed output of living doc script

--- Scripts/CreateDoc.bat
+++ Scripts/CreateDoc.bat
@@ -1,1 +1,1 @@
-livingdoc test-assembly c:\SoftwareProjects\OnspringAutomatedUITests\bin\Debug\net6.0\OnspringAutomatedUITests.dll -t c:\SoftwareProjects\OnspringAutomatedUITests\bin\Debug\net6.0\TestExecution.json --output c:\SoftwareProjects\OnspringAutomatedUITests\index.html
+livingdoc test-assembly c:\SoftwareProjects\OnspringAutomatedUITests\bin\Debug\net6.0\OnspringAutomatedUITests.dll -t c:\SoftwareProjects\OnspringAutomatedUITests\bin\Debug\net6.0\TestExecution.json --output c:\SoftwareProjects\OnspringAutomatedUITests\docs\index.html
